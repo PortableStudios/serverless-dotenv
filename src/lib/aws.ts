d6996fc163938e10a21c67d09bfdfeaa463b1fc3
Chris D'Aloisio <chris.daloisio@portable.com.au>
2019-11-19 02:04:50 +1100

    Starting to add the CloudFormation export fetching

--- src/lib/aws.ts
+++ src/lib/aws.ts
@@ -1,8 +1,19 @@
+import AWS from 'aws-sdk'
+
 import { Options } from './cli'
 
-export async function fetchExports(mappings: any, options: Options) {
-  const { profile, stage } = options
-  console.log(profile)
-  console.log(stage)
-  console.log(mappings)
+function initCloudFormation(profile: string): AWS.CloudFormation {
+  const credentials = new AWS.SharedIniFileCredentials({ profile });
+
+  AWS.config.credentials = credentials;
+  AWS.config.region = 'ap-southeast-2';
+
+  return new AWS.CloudFormation();
+}
+
+export async function fetchExports(_mappings: any, options: Options) {
+  const { profile } = options
+  const cloudFormation = initCloudFormation(profile)
+
+  await cloudFormation.listExports().promise()
 }
